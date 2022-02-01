e6d1fae88ef309dd1daee3ad8983a19a96488362
KayoSilva19 <Kayohenriquesilva19@gmail.com>
2022-01-31 21:38:19 -0300

    AulaDia31/01/22

---
+++ Modulo01-Logica/Aula_02/Operador_NAO/js/script.js
@@ -0,0 +1,10 @@
+//Leia um login e se este não for o administrador exiba a 
+//mensagem: SEM PODERES ADMINISTRATIVOS.
+
+let usuario=prompt("Usuário");
+if(!(usuario=="Administrador")){
+    alert("Sem poderes administrativos!");
+}else {
+    alert("Ola Administrador!")
+}
+
--- Modulo01-Logica/Aula_02/jogoONG_03/js/script.js
+++ Modulo01-Logica/Aula_02/jogoONG_03/js/script.js
@@ -3,6 +3,7 @@ let ag=parseFloat(prompt("Digite a nota AG"));
 let at=parseFloat(prompt("Digite a nota AT"));
 
 let media = (ac * 20 + ag * 10+ at *70 ) / 100;
+let media = (ac * .20 + ag * 0.10+ at *0.70 ) / 100;
 
 alert(`Sua média é ${media}`)
 
